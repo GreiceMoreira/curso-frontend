fb60d6670a4abcbd2ab68d0d378d21f6f51b1576
Greice Moreira <greice.f.d.a@hotmail.com>
2023-05-06 21:53:53 -0400

    problemas com gulp

--- Gulp/projeto/gulpfile.js
+++ Gulp/projeto/gulpfile.js
@@ -3,11 +3,19 @@ const concat = require('gulp-concat')
 const cssmin = require('gulp-cssmin') 
 const rename = require('gulp-rename')
 const uglify = require('gulp-uglify')
+const image = require('gulp-image')
+// image = import("gulp-image")
 
 function tarefasCSS(cb) {
 
-    return gulp.src('./vendor/**/*.css')
-        .pipe(concat('libs.css'))
+    return gulp.src([
+        './node_modules/bootstrap/dist/css/bootstrap.css',
+        './node_modules/@fortawesome/fontawesome-free/css/fontawesome.css',
+        './vendor/owl/css/owl.css',
+        './vendor/jquery-ui/jquery-ui.css',
+        './src/css/style.css'
+    ])
+        .pipe(concat('styles.css'))
         .pipe(cssmin())
         .pipe(rename({ suffix: '.min'})) // libs.min.css
         .pipe(gulp.dest('./dist/css'))
@@ -16,13 +24,40 @@ function tarefasCSS(cb) {
 
 function tarefasJs() {
 
-return gulp.src('./vendor/**/*.js')
-        .pipe(concat('libs.js'))
-        .pipe(uglify())
-        .pipe(rename({ suffix: '.min'})) //libs.min.js
-        .pipe(gulp.dest('./dist/js'))
+    return gulp.src([
+        './node_modules/jquery/dist/jquery.js',
+        './node_modules/bootstrap/dist/js/bootstrap.js',
+        './vendor/owl/js/owl.js',
+        './vendor/jquery-mask/jquery.mask.js',
+        './vendor/jquery-ui/jquery-ui.js',
+        './src/js/custom.js'
+    ])
+            .pipe(concat('scripts.js'))
+            .pipe(uglify())
+            .pipe(rename({ suffix: '.min'})) //libs.min.js
+            .pipe(gulp.dest('./dist/js'))
+    
+    }
 
+function tarefasImagem(){
+    
+    return gulp.src('./src/images/*')
+        .pipe(image({
+            pngquant: true,
+            optipng: false,
+            zopflipng: true,
+            jpegRecompress: false,
+            mozjpeg: true,
+            gifsicle: true,
+            svgo: true,
+            concurrent: 10,
+            quiet: true
+        }))
+        .pipe(gulp.dest('./dist/images'))
 }
+
+
 
 exports.styles = tarefasCSS
 exports.scripts = tarefasJs
+exports.images = tarefasImagem
