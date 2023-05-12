7514b53fc4826a7419f7d43b68f3e647df5bce94
Greice Moreira <greice.f.d.a@hotmail.com>
2023-05-12 14:18:25 -0400

    gulp2

--- Gulp/projeto/gulpfile.js
+++ Gulp/projeto/gulpfile.js
@@ -1,9 +1,16 @@
+const { series, parallel} = require('gulp')
 const gulp = require('gulp')
 const concat = require('gulp-concat')
 const cssmin = require('gulp-cssmin')
 const rename = require('gulp-rename')
 const uglify = require('gulp-uglify')
 const image = require('gulp-image')
+const stripJs = require('gulp-strip-comments')
+const stripCss = require('gulp-strip-css-comments')
+const htmlmin = require('gulp-htmlmin')
+const babel = require('gulp-babel')
+const browserSync = require('browser-sync').create()
+const reload = browserSync.reload
 
 function tarefasCSS(cb) {
 
@@ -53,6 +60,34 @@ function tarefasImagem(){
         .pipe(gulp.dest('./dist/images'))
 }
 
+// POC - Proof of Concept
+function tarefasHTML(callback){
+
+    gulp.src('./src/**/*.html')
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest('./dist'))
+
+    return callback()
+
+}
+
+gulp.task('serve', function(){
+
+    browserSync.init({
+        server: {
+            baseDir: "./dist"
+        }
+    })
+    gulp.watch('./src/**/*').on('change', process) // repete o processo quando alterar algo em src
+    gulp.watch('./src/**/*').on('change', reload)
+
+})
+
+// series x parallel
+const process = series( tarefasHTML, tarefasJS, tarefasCSS)
+
 exports.styles = tarefasCSS
 exports.scripts = tarefasJS
 exports.images = tarefasImagem
+
+exports.default = process
