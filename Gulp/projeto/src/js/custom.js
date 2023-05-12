6d2cad8a629a88b463b7be2907330686bdeb73d6
Greice Moreira <greice.f.d.a@hotmail.com>
2023-05-11 22:09:23 -0400

    gulp1

--- Gulp/projeto/src/js/custom.js
+++ Gulp/projeto/src/js/custom.js
@@ -1,21 +1,21 @@
 // instancia jquery e evita conflitos
 // jQuery( function($){
-$(document).ready(function(){
-   
+$(document).ready(function () {
+
    $('.owl-carousel').owlCarousel();
-   
+
    let titulos = $('h4') // tag
-   
+
    let itens = $('.featured-item') // class
-   
+
    let destaques = $('#featured') // id
-   
+
    // console.log(titulos.first());
-   
+
    // Configuração de produtos
-   
+
    $('.featured-item a').addClass('btn btn-dark stretch-link');
-   
+
    $('.featured-item:first h4').append('<span class="badge bg-secondary">Novo</span>')
    // $('.featured-item:first h4').start('<span class="badge bg-secondary">Novo</span>')
    // $('.featured-item:first h4').html('<span class="badge bg-secondary">Novo</span>')
@@ -28,65 +28,61 @@ $(document).ready(function(){
    // $('.featured-item:first h4').fadeOut()
    //  $('.featured-item:first h4').css('color', '#f00')
 
-   
-   $('.featured-item h4').dblclick( function(){
-      
+
+   $('.featured-item h4').dblclick( function (){
+
       $(this).css({
          'color': '#f00',
          'background': '#ff0',
          'font-weight': '100',
       });
-      
+
    });
-   
+
    /*
     * Manipulação de eventos
     */
-   $('.featured-item a').on('blur', function(event){
-      
+   $('.featured-item a').on('blur', function (event) {
+
       event.preventDefault();
-      
+
       alert('Produto esgotado');
-      
+
    })
 
    /* 
-    * Callback
-    * entendendo ações que começam ao termino de outra
-    */
+       * Callback
+       * entendendo ações que começam ao termino de outra
+     
    $('.featured-item:nth(1)')
-      .hide(500, function(){
+      .hide(500, function () {
          // este é o callback
-         console.log( $(this).find('h4').text() + ' esgotado')
+         console.log($(this).find('h4').text() + ' esgotado')
       })
-      .show(500, function(){
-         console.log( $(this).find('h4').text() + ' em estoque')
+      .show(500, function () {
+         console.log($(this).find('h4').text() + ' em estoque')
       })
- 
+   */
 
    /*
     * Animações
     */
-   const duracao = 1000 // equivalenta a 1 segundo
-
-   $('.featured-item:nth(0)')
-      .hide(duracao)
-      .show(duracao)
-      .fadeOut(duracao)
+   const duracao = 4000 // equivalenta a 1 segundo
+   const inicio = 0001
+   $('.featured-item')
+      .hide(inicio)
       .fadeIn(duracao)
-      .toggle(duracao)
-      .toggle(duracao)
 
-   $('#form-submit').on('click', function(e){
+   $('#form-submit').on('click', function (e) {
 
       e.preventDefault()
 
-      if( $('#email').val() != '' ){
+      if ($('#email').val() != '') {
 
          $('#email').animate({
             opacity: "toggle",
             top: "-50"
-         }, 500, function(){
+         }, 500, function () {
             console.log($(this).val())
          })
 
@@ -99,14 +95,14 @@ $(document).ready(function(){
    /*
     * Ouvinte de eventos .nav-modal-open
     */
-   $('.nav-modal-open').on('click', function(e){
+   $('.nav-modal-open').on('click', function (e) {
 
       e.preventDefault();
 
       let elem = $(this).attr('rel')
 
-      $('.modal-body').html($('#'+elem).html())
-      
+      $('.modal-body').html($('#' + elem).html())
+
       $('.modal-header h5.modal-title').html($(this).text())
 
       let myModal = new bootstrap.Modal($('#modelId'))
@@ -123,10 +119,10 @@ $(document).ready(function(){
     * - checar se o email é válido com ao menos um "@" e "."
     * - checar se o cpf é válido com regex
     */
-   function validate( elem ){
-      if( elem.val() == '') {
+   function validate(elem) {
+      if (elem.val() == '') {
 
-         console.log('o campo de '+ elem.attr('name') + ' é obrigatório')
+         console.log('o campo de ' + elem.attr('name') + ' é obrigatório')
 
          elem.parent().find('.text-muted').show()
 
@@ -139,7 +135,7 @@ $(document).ready(function(){
       }
    }
 
-   $('body').on('submit', '.modal-body .form', function(e){
+   $('body').on('submit', '.modal-body .form', function (e) {
 
       e.preventDefault()
 
@@ -149,51 +145,54 @@ $(document).ready(function(){
       validate(inputName)
       validate(inputEmail)
 
-      if(inputEmail.hasClass('invalid') || inputName.hasClass('invalid')){
+      if (inputEmail.hasClass('invalid') || inputName.hasClass('invalid')) {
          console.log('verificar campos obrigatórios')
          return false
       } else {
-         $(this).submit()  
+         $(this).submit()
       }
 
    })
 
-   $('body').on('blur', '#nome', function(){
+   $('body').on('blur', '#nome', function () {
       validate($(this))
    })
 
-   $('body').on('blur', '#email', function(){
+   $('body').on('blur', '#email', function () {
       validate($(this))
+ 
    })
 
 
-   $('body').on('focus', '#date', function(){
+   $('body').on('focus', '#date', function () {
       $(this).datepicker()
    })
 
-   $('body').on('blur', '#date', function(){
+   $('body').on('blur', '#date', function () {
       validate($(this))
       $(this).mask('00/00/0000');
-   }) 
+   })
 
-   $('body').on('blur', '#time', function(){
+   $('body').on('blur', '#time', function () {
       validate($(this))
       $(this).mask('00:00');
    })
 
-   $('body').on('blur', '#cep', function(){
+   $('body').on('blur', '#cep', function () {
       validate($(this))
       $(this).mask('00000-000');
    })
 
-   $('body').on('blur', '#phone', function(){
+   $('body').on('blur', '#phone', function () {
       validate($(this))
       $(this).mask('00000-0000');
    })
 
-   $('body').on('blur', '#cpf', function(){
+   $('body').on('blur', '#cpf', function () {
       validate($(this))
       $(this).mask('000.000.000-00');
    })
+
+
 
 })
