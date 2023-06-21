4ce2d3f7c12caf0ad64001298c3fb804514e2118
Greice Moreira <greice.f.d.a@hotmail.com>
2023-06-21 14:57:56 -0400

    commit

---
+++ JS-adv/src/works.js
@@ -0,0 +1,46 @@
+let works = [
+    {   
+        tecnica: 'Crochet',
+        nomeReceita: 'Kimono alfa',
+        fio: ['algodao','sintetico'],
+        quantidadeNovelo: 3,
+        horas: 8
+    },
+    {   
+        tecnica: 'Tricot',
+        nomeReceita: 'Xale beta',
+        fio: 'sintetico',
+        quantidadeNovelo: 4,
+        horas: 10
+    },    {   
+        tecnica: 'Crochet',
+        nomeReceita: 'Flor circular',
+        fio: 'algodao',
+        quantidadeNovelo: 1,
+        horas: 6
+    },    {   
+        tecnica: 'Crochet',
+        nomeReceita: 'Tapete quadrado',
+        fio: 'algodao',
+        quantidadeNovelo: 5,
+        horas: 30
+    },    {   
+        tecnica: 'Tricot',
+        nomeReceita: 'Vestido verao',
+        fio: 'sintetico',
+        quantidadeNovelo: 6,
+        horas: 70
+    },    {   
+        tecnica: 'Crochet',
+        nomeReceita: 'Saia midi',
+        fio: 'algodao',
+        quantidadeNovelo: 5,
+        horas: 45
+    },
+]
+
+function getWorks(){
+    return works
+}
+
+export default getWorks()
